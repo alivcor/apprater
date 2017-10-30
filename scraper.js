bbe9473b8a49899e5ba53965daec13157791e8a3
alivcor <abhinandandubey@live.com>
2017-10-30 19:32:21 -0400

    modified node script

--- scraper.js
+++ scraper.js
@@ -1,33 +1,14 @@
 var gplay = require('google-play-scraper');
 var fs = require('fs')
 var Promise = require('promise');
-
+var myArgs = process.argv.slice(2);
+var passed_appid = myArgs[0];
 var read = Promise.denodeify(fs.readFile)
 var write = Promise.denodeify(fs.writeFile)
 
-fs.readFile('appid.txt',function read(err,data) {
-	if (err) {
-        throw err;
-    }
-    
-    content = data;
-    for (x in content) {
-    	console.log(x);
-    	//jsonwriter(x);
-    }
-    // Invoke the next step here however you like
-    //console.log(content);
-       // Put all of the code here (not the best solution)
-    //jsonwriter('com.jolansky.glazesimplicity.theme');          // Or put the next step in a function and invoke it
-
-	// body...
-});
-
-function jsonwriter(appid){
-
-gplay.app({appId: appid})
+gplay.app({appId: passed_appid})
   .then(function (str) {
-    return write('foo.json', JSON.stringify(str, null, '  '), 'utf8')
+    console.log(str);
+    return write(passed_appid + '.json', JSON.stringify(str, null, '  '), 'utf8');
   })
-
-}
+process.exit();
