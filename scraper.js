04ddfab8aaf27c9bdb68a81b2daefcdb900826f6
alivcor <abhinandandubey@live.com>
2017-10-30 19:40:58 -0400

    fixed process exit error

--- scraper.js
+++ scraper.js
@@ -3,12 +3,12 @@ var fs = require('fs')
 var Promise = require('promise');
 var myArgs = process.argv.slice(2);
 var passed_appid = myArgs[0];
-var read = Promise.denodeify(fs.readFile)
-var write = Promise.denodeify(fs.writeFile)
+console.log(passed_appid);
+var read = Promise.denodeify(fs.readFile);
+var write = Promise.denodeify(fs.writeFile);
 
 gplay.app({appId: passed_appid})
   .then(function (str) {
-    console.log(str);
-    return write(passed_appid + '.json', JSON.stringify(str, null, '  '), 'utf8');
+    return write(passed_appid + '.json', JSON.stringify(str, null, '  '), 'utf8')
   })
-process.exit();
+.then(function (){process.exit()});
