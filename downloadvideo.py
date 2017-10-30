da402de2315c66bd7265a9497fa4ea888b1bdf7f
Praneeth G <praneethg@Praneeths-MacBook-Pro.local>
2017-10-30 19:52:10 -0400

    testing

--- downloadvideo.py
+++ downloadvideo.py
@@ -1,12 +1,25 @@
 import json
 from pprint import pprint
 import youtube_dl
+import Constants
+import os
 
+path=getcwd()
+App_Id_File_Path = os.path.join(path+'/'+Constants.App_Id_File_Path)
+count =0
 
-data = json.loads(open("foo.json","r").read())
-video_url = data['video']
-print data['video']
-print "Downloading!"
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([video_url])
+with open(App_Id_File_Path) as f:
+	Appid = f.readlines()
+Appid = [x.strip() for x in Appid]
+
+for app in Appid:
+	count = count+1
+	node_command = "node " + app + " "+ str(count) 
+	os.system(node_command)
+	data = json.loads(open(str(count)+".json","r").read())
+	video_url = data['video']
+	print data['video']
+	print "Downloading!"
+	ydl_opts = {}
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    	ydl.download([video_url])
