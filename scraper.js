cbdec8b22720e9d5c8d5b281f773ed63d226591d
alivcor <abhinandandubey@live.com>
2017-10-30 19:44:40 -0400

    counter

--- scraper.js
+++ scraper.js
@@ -3,12 +3,13 @@ var fs = require('fs')
 var Promise = require('promise');
 var myArgs = process.argv.slice(2);
 var passed_appid = myArgs[0];
+var passed_appcount = myArgs[1];
 console.log(passed_appid);
 var read = Promise.denodeify(fs.readFile);
 var write = Promise.denodeify(fs.writeFile);
 
 gplay.app({appId: passed_appid})
   .then(function (str) {
-    return write(passed_appid + '.json', JSON.stringify(str, null, '  '), 'utf8')
+    return write(passed_appcount + '.json', JSON.stringify(str, null, '  '), 'utf8')
   })
 .then(function (){process.exit()});
