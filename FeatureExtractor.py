ee3d9f72640ba46abafb2490ece055bb21a82070
alivcor <abhinandandubey@live.com>
2017-11-14 21:32:03 -0500

    Working !

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -3,23 +3,21 @@ from vgg16_feat import VGG16
 from keras.models import Model
 
 
-def _preprocess_input(input_tensors):
-    print "Preprocessing Input Frames..."
-    processed_input_tensors = []
-
-    for input_tensor in input_tensors:
-        im = cv2.resize(input_tensor, (224, 224)).astype(np.float32)
-        im = np.expand_dims(im, axis=0)
-        processed_input_tensors.append(im)
-
+def _preprocess_input(input_tensor):
+    print "Preprocessing Input Frame..."
+    processed_input_tensor = cv2.resize(input_tensor, (224, 224)).astype(np.float32)
+    processed_input_tensor = np.expand_dims(processed_input_tensor, axis=0)
     print "Preprocessing Complete !"
-    processed_input_tensors = np.array(processed_input_tensors)
-    print "processed_input_tensors.shape : ", processed_input_tensors.shape
-    return processed_input_tensors
+    print "processed_input_tensor.shape : ", processed_input_tensor.shape
+    return processed_input_tensor
 
 
-def load_extractor_model():
+def init_load_extractor_model():
+    print "Loading Extractor Model.."
+
     vgg_model = VGG16(include_top=True, weights='imagenet')
+
+    print "Shredding Softmax Layer.."
     vgg_feature_vector = vgg_model.layers[-2].output
     # print vgg_feature_vector.shape
 
@@ -29,10 +27,14 @@ def load_extractor_model():
 
 
 def extract_features(model, input_tensors):
-    input_tensors = _preprocess_input(input_tensors)
+    feature_vectors = []
 
     print "Extracting Features.."
-    feature_vectors = model.predict(input_tensors)
+
+    for input_tensor in input_tensors:
+        processed_input_tensor = _preprocess_input(input_tensor)
+        feature_vector = model.predict(processed_input_tensor)
+        feature_vectors.append(feature_vector)
 
     print "Features Extracted !"
-    return feature_vectors
+    return np.array(feature_vectors)
--- apprater_nn.py
+++ apprater_nn.py
@@ -5,21 +5,21 @@ from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D
 
 
-
-
-
 cap = cv2.VideoCapture('dataset/dataset_100/1/Modern Combat 5 - Game Trailer-_jSPJWqQM90.mp4')
 # # Read the first frame of the video
 ret, frame = cap.read()
 print frame.shape
 
-vgg_features = FeatureExtractor.extract_features(frame)
+feature_extraction_model = FeatureExtractor.init_load_extractor_model()
+vgg_features = FeatureExtractor.extract_features(feature_extraction_model, [frame])
 
-apprater_model = Sequential()
-apprater_model.add(Dense(12, activation='relu', name='rating'))
-apprater_model.add(Dense(8, activation='tanh'))
-apprater_model.add(Dense(8, activation='relu'))
-apprater_model.add(Dense(5, activation='softmax'))
-apprater_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-apprater_model.summary()
-
+print vgg_features.shape
+#
+# apprater_model = Sequential()
+# apprater_model.add(Dense(12, activation='relu', name='rating'))
+# apprater_model.add(Dense(8, activation='tanh'))
+# apprater_model.add(Dense(8, activation='relu'))
+# apprater_model.add(Dense(5, activation='softmax'))
+# apprater_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# apprater_model.summary()
+#
