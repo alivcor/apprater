f9558609d0e0c6a0ad17fb45609cbbda56d32559
alivcor <abhinandandubey@live.com>
2017-11-15 02:19:22 -0500

    Running

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from keras.preprocessing import image
-from keras.applications.imagenet_utils import decode_predictions, preprocess_input
-import numpy as np
+import cv2, numpy as np
+import sys, time, datetime
+import FeatureExtractor, EventIssuer
+import progressbar
+from keras.layers import Flatten, Dense, Input
+from keras.models import Sequential
+import glob, os, pickle
+from keras.layers import Convolution2D, MaxPooling2D
 
-img_path = 'elephant.jpg'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-print('Input image shape:', x.shape)
+
+
+def compileMainModel():
+    apprater_model = Sequential()
+    apprater_model.add(Dense(12, input_dim=11, activation='relu', name='rating'))
+    apprater_model.add(Dense(8, activation='tanh'))
+    apprater_model.add(Dense(8, activation='relu'))
+    apprater_model.add(Dense(5, activation='softmax'))
+    apprater_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    apprater_model.summary()
+
+compileMainModel()
